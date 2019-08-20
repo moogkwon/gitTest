ef1256653f72954bc7a72ea63fc3a5aa1d3b0246
MOOG <MOOG@DESKTOP-3AHBOU3>
2019-08-20 13:49:08 +0900

    model Class added

--- GitProject/src/com/git/run/Main.java
+++ GitProject/src/com/git/run/Main.java
@@ -3,9 +3,7 @@ package com.git.run;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("안녕, 내가 만든 출력이야!");
-		String name = "유병승";
-		String yo = "yoyoyo";
+		System.out.println("yo");
 	}
 
 }
