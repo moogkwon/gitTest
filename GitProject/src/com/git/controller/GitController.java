7bbe052d559ceede875a7af8bc147c14460a2005
MOOG <MOOG@DESKTOP-3AHBOU3>
2019-08-20 15:00:52 +0900

    created a package and a class

---
+++ GitProject/src/com/git/controller/GitController.java
@@ -0,0 +1,17 @@
+package com.git.controller;
+
+public class GitController {
+	
+	
+	public void printGit() {
+		System.out.println("wow, I'm using git");
+	}
+	
+	public int calculator(int a, int b) {
+		return a+b;
+	}
+	
+	
+	
+	
+}
